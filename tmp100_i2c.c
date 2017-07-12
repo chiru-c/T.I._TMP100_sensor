1e9f5ac03c99abac796911941d36ab2745534bcb
Cezar Chiru <cezarx.chiru@intel.com>
2017-07-12 16:46:09 +0300

    Re-arrange the out of range conditions in file.
    Description:
        - Moved the range conditions up in the file
        - Add comments to code
        - Extend name of variables for better readability
        - Aligned the code and removed the trailing whitespaces

--- tmp100_i2c.c
+++ tmp100_i2c.c
@@ -6,11 +6,15 @@
 #include <sys/ioctl.h>
 #include <getopt.h>
 
-int cr, th, tl, address;
 
 int main (int argc, char ** argv)
 {
+    int conf_reg, temp_high, temp_low, address, file;
     char c;
+    float t;
+    __u8 reg = 0x00;
+    __s32 res, b1, b0;
+
     while (1){
         c = getopt (argc, argv, "A:C:L:H:");
         if (c == -1){
@@ -23,66 +27,70 @@ int main (int argc, char ** argv)
             printf ("The selected chip-address is: %s\n", optarg);
             break;
         case 'C':
-            cr = atoi(optarg);
-            printf ("The argument cr is %s !\n", optarg);
+            conf_reg = atoi(optarg);
+            printf ("The CONFIG_REGISTER value is: %s!\n", optarg);
             break;
         case 'L':
-            tl = atoi(optarg);
-            printf ("The argument TL is set to %s .\n", optarg);
+            temp_low = atoi(optarg);
+            printf ("The TEMP_LOW_REGISTER value is: %s.\n", optarg);
             break;
         case 'H':
-            th = atoi(optarg);
-            printf ("The argument TH is set to %s .\n", optarg);
+            temp_high = atoi(optarg);
+            printf ("The TEMP_HIGH_REGISTER value is: %s.\n", optarg);
             break;
-        case '?':
         default:
-            printf("Usage: %s -A [chip-address] -C [config-reg] -L [Tlow-reg] -H [Thigh-reg] \n",argv[0]);
-            break;
+            printf("Usage: %s -A [chip-address] -C [config-reg] -L [Tlow-reg] -H [Thigh-reg] \n", argv[0]);
+            exit(1);
         }
     }
 
-    printf ("The value stored in the parameters: %#04x %#04x %#04x %#04x\n", address, cr, tl, th);
+    //Check the values that you have entered in a hex format
+    printf ("The value stored in the parameters: %#04x %#04x %#04x %#04x\n", address, conf_reg, temp_low, temp_high);
 
-    int file;
+    //address = 0x48;
+
+    //Test out of range values
+    if (address < 0  || address > 119){
+        printf ("Chip address is out of 0x00 - 0x77 range!\n");
+        exit(-1);
+    }
+
+    if ( conf_reg > 255 || conf_reg < 0 ){
+        printf ("The Config register value is out of range!\n");
+        exit (-1);
+    } else {
+       i2c_smbus_write_word_data(file, 0x01, conf_reg);
+    }
+
+    if ( temp_low > 65535 || temp_low < 0 ){
+        printf ("The TempLow register value is out of range!\n");
+        exit (-1);
+    } else {
+        i2c_smbus_write_word_data(file, 0x02, temp_low);
+    }
+
+    if ( temp_high > 65535 || temp_high < 0 ){
+        printf ("The TempHigh register value is out of range!\n");
+        exit (-1);
+    } else {
+        i2c_smbus_write_word_data(file, 0x03, temp_high);
+    }
+
+    // Open the i2c device
     file = open("/dev/i2c-1", O_RDWR);
     if (file < 0){
         printf("ERROR open(%d)failed\n", file);
         exit(1);
     }
 
-    //int addr = 0x48;
-
+    //Specify address of the i2c chip to communicate with
     if (ioctl(file, I2C_SLAVE, address) < 0){
         printf("IOCTL failed\n");
         exit(1);
     }
-    __u8 reg = 0x00;
-    __s32 res, b1, b0;
 
-    if ( cr > 255 || cr < 0 ){
-        printf ("The Config register value is out of range!\n");
-        exit (-1);
-    } else {
-       i2c_smbus_write_word_data(file, 0x01, cr);
-    }
-
-    if ( tl > 65535 || tl < 0){
-        printf ("The TempLow register value is out of range!\n");
-        exit (-1);
-    } else {
-        i2c_smbus_write_word_data(file, 0x02, tl);
-    }
-
-    if ( th > 65535 || th < 0){
-        printf ("The TempHigh register value is out of range!\n");
-        exit (-1);
-    } else {
-        i2c_smbus_write_word_data(file, 0x03, th);
-    }
-
+    //Read the Temperature register
     res = i2c_smbus_read_word_data(file, reg);
-
-    float t;
 
     if (res < 0){
         printf("SMBus read failed\n");
